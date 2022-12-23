dc18a29127f1d26372f2875daca9fed35ce64061
Adrian Mincu <mal13adi03@gmail.com>
2022-12-23 13:00:43 +0200

    correct input for nr of nodes

--- 132_Mincu_AdrianLucian_0.s
+++ 132_Mincu_AdrianLucian_0.s
@@ -3,7 +3,7 @@
     fs1: .asciz "%ld "
     fs2: .asciz "\n"
     fs3: .asciz "%ld\n"
-    matrice: .space 400
+    matrice: .space 40000
     linei: .space 4
     coloana: .space 4
     n: .space 4
@@ -13,20 +13,51 @@
 
 citire:
     pushl %ebp
+    pushl %edi
     mov %esp,%ebp
-
+    
     # n= 12(%ebp)
     # matrice = 8(%ebp)
 
-    pushl 12(%ebp)
+    lea 12(%ebp),%edi
+    pushl %edi
     pushl $fs
     call scanf
     addl $8,%esp
 
-    
+    pushl 12(%ebp)
+    pushl $fs3
+    call printf
+    addl $8,%esp
 
-    popl %ebp
-    ret
+    # -400(ebp) = legaturi
+    addl $400,%esp
+    lea -400(%ebp),%edi
+    movl $0,%ecx
+
+    # -404(%ebp) = variabila temporara
+    addl $4,%esp
+    movl $3,-404(%ebp)
+
+    pushl 12(%ebp)
+    pushl $fs3
+    call printf
+    addl $8,%esp
+
+    citire_legaturi:
+        cmp 12(%ebp),%ecx
+        je iesire
+
+
+
+        addl $1,%ecx
+        jmp citire_legaturi
+
+    iesire:
+        subl $404,%esp
+        popl %edi
+        popl %ebp
+        ret
 
 .global main
 
@@ -51,6 +82,8 @@ cerinta_1:
     pushl $matrice
     call citire
     addl $8,%esp
+
+    jmp exit
 
 cerinta_2:
     pushl $n
